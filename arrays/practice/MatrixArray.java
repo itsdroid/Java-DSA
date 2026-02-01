640cf82386bbddfb12108a8967c21200877aa610
itsdroid <shubhampopalghat77@gmail.com>
2026-02-01 10:45:27 +0530

    implemented MatrixArray.java

---
+++ arrays/practice/MatrixArray.java
@@ -0,0 +1,40 @@
+package arrays.practice;
+
+import java.util.Scanner;
+
+public class MatrixArray {
+    static void readMatrix(int[][] matrix, int rows, int cols, Scanner sc) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+    }
+
+    static void printMatrix(int[][] matrix, int rows, int cols) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        System.out.println("Enter the matrix elements: ");
+        readMatrix(matrix, rows, cols, sc);
+
+        System.out.println("Your matrix: ");
+        printMatrix(matrix, rows, cols);
+    }
+}
