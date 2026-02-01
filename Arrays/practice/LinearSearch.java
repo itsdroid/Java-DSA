63f3bf39f8ac82c471ddcde643ea7faf5858ebb0
itsdroid <shubhampopalghat77@gmail.com>
2026-02-01 10:12:15 +0530

    updated LinearSearch with proper method

--- Arrays/practice/LinearSearch.java
+++ Arrays/practice/LinearSearch.java
@@ -1,18 +1,31 @@
-package Arrays.practice;
+package arrays.practice;
+
 import java.util.Scanner;
 
 public class LinearSearch {
+
+    static int linearSearch(int[] arr, int x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int arr[] = {58,46,65,77,25,90,12};
+        int arr[] = { 58, 46, 65, 77, 25, 90, 12 };
 
         System.out.print("Enter the number you want to search: ");
         int x = sc.nextInt();
-        for( int i = 0; i < arr.length; i++) {
-            if(x == arr[i]) {
-                System.out.println(x + " is at position: " + i );
-                break;
-            }
+
+        int index = linearSearch(arr, x);
+
+        if (index != -1) {
+            System.out.println(x + " is at index: " + index);
+        } else {
+            System.out.println(x + " not found in the array");
         }
         sc.close();
     }
