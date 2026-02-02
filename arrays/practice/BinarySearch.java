9411b02e991076f137313138bda112edb3f7456f
itsdroid <shubhampopalghat77@gmail.com>
2026-02-02 17:49:31 +0530

    implemented BinarySearch.java

--- arrays/practice/BinarySearch.java
+++ arrays/practice/BinarySearch.java
@@ -5,22 +5,23 @@ public class BinarySearch {
         int start = 0;
         int end = numbers.length-1;
 
-        while (start <= end) {
-            int mid = (start+end) / 2;
-            if(numbers[mid] == key) {
-                return mid;
-            }
-            if ( numbers[mid] < key ) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
+        while(start <= end) {
+        int mid = (start+end)/2;
+        if(numbers[mid]==key) {
+            return mid;
         }
+        if(numbers[mid] < key) {
+            start = mid+1;
+        }
+        else {
+            end = mid-1;
+        }
+    }
         return -1;
     }
 
     public static void main(String[] args) {
-        int[] numbers = {2,6,7,3,20,25};
+        int[] numbers = {2,4,7,9,12,17,22};
         int key = 7;
 
         System.out.println("index of key is: " + binarySearch(numbers, key));
